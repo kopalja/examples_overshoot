d1f4c41b7de336d9d4fa5ddb80dbe4e1ba53798e
kopalja <kopal.jakub@gmail.com>
2025-05-26 14:11:05 +0200

    .

--- vae/run.sh
+++ vae/run.sh
@@ -2,11 +2,13 @@
 set -xe
 
 
-
 SEEDS=(10 20 30 40 50 60 70 80 90 100)
-for seed in ${SEEDS}; do
+for seed in "${SEEDS[@]}"; do
+    echo "Processing seed: `${seed}`"
     python main.py --seed ${seed} --job_name vae_full_run --optimizer_name adam
-    python main.py --seed ${seed} --job_name vae_full_run --optimizer_name adamo
+    python main.py --seed ${seed} --job_name vae_full_run --optimizer_name adamo --overshoot 3
+    python main.py --seed ${seed} --job_name vae_full_run --optimizer_name adamo --overshoot 5
+    python main.py --seed ${seed} --job_name vae_full_run --optimizer_name adamo --overshoot 7
 done
 
 
